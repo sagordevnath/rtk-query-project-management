4c025956632ddfe3c7694a3e212a4993b8d651c6
sagor devnath <ssagor64@gmail.com>
2022-10-03 14:43:16 +0600

    change server link

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedOut } from "../auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+    baseUrl:"https://project-management--server.herokuapp.com",
     prepareHeaders: async (headers, { getState, endpoint }) => {
         const token = getState()?.auth?.accessToken;
         if (token) {
